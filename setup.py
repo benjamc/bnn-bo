8344b91773254215483f430d7144b23c98c74c71
benjamc <c.benjamins@ai.uni-hannover.de>
2025-11-23 11:12:25 +0100

    fix packaging

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(name='bnn_bo',
       version='0.1',
-      packages=find_packages(include=['test_functions']),
+      packages=find_packages(include=['bnnbo_test_functions']),
       install_requires=[
             'botorch',
             'gpytorch'
